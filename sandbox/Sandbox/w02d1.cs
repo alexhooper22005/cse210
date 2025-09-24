f3b67a3f3dfdd2ecf7627bc9fe6b1000251ced03
alexhooper22005 <hoo22005@byui.edu>
2025-09-24 10:49:22 -0600

    added notes

---
+++ sandbox/Sandbox/w02d1.cs
@@ -0,0 +1,41 @@
+using System;
+
+class program
+{
+    static void main(string[] args)
+    {
+        List<string> fruits = new List<string>() { "Apple", "Banana", "Cherry" };
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        fruits.Add("Date");
+        fruits.Add("Eggplant");
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"i: {i}");
+        }
+
+        Console.Write("Make a choice (y or n): ");
+        string value = Console.ReadLine().ToUpper();
+        while (value != "Y" && value != "N") ;
+        {
+            Console.Write("Choose y or n: ");
+            value = Console.ReadLine().ToUpper();
+        }
+
+        // string value;
+        do
+        {
+            Console.Write("Enter y or n: ");
+            value = Console.ReadLine().ToUpper();
+
+        } while (value != "Y" && value != "N");
+
+        
+
+
+    }
+}
