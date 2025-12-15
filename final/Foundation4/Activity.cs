750b898c130f3ad9068a141db992c0a2b0e69b81
alexhooper22005 <hoo22005@byui.edu>
2025-12-15 12:15:54 -0700

    deleted unused code

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -3,7 +3,7 @@ public abstract class Activity
     protected DateOnly _date;
     protected double _length;
 
-    // private string summary = _date.ToString("dd MMM yyyy");
+  
     public Activity(DateOnly date, double length)
     {
         _date = date;
